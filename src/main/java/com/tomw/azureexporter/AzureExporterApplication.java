f8fcaea7ba06e51ab6049d21a90192dae54bdd82
Tom <T_o_m_white@hotmail.com>
2022-10-21 21:07:09 +0100

    Initial commit.

---
+++ src/main/java/com/tomw/azureexporter/AzureExporterApplication.java
@@ -0,0 +1,13 @@
+package com.tomw.azureexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomw/azureexporter/AzureExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomw.azureexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
