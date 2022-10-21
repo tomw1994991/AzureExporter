d8c7cafc665dbc39d97f9349f72d6cc536ac0a47
Tom <T_o_m_white@hotmail.com>
2022-10-21 22:41:58 +0100

    Add actuator endpoint inc prometheus.

---
+++ src/main/java/com/tomw/azureexporter/PrometheusScrapeEndpointWebExtension.java
@@ -0,0 +1,31 @@
+package com.tomw.azureexporter;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
+import org.springframework.boot.actuate.metrics.export.prometheus.TextOutputFormat;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@EndpointWebExtension(endpoint = PrometheusScrapeEndpoint.class)
+@Slf4j
+@AllArgsConstructor
+public class PrometheusScrapeEndpointWebExtension {
+
+    private PrometheusScrapeEndpoint delegate;
+    private MeterRegistry registry;
+
+    @ReadOperation
+    public WebEndpointResponse<String> scrape(TextOutputFormat format, @Nullable Set<String> includedNames) {
+        log.info("Prometheus scrape endpoint hit.");
+        //Counter myCounter = Counter.builder("custom.metric.name").tag("key1", "val1").description("custom_metric_desc").register(registry);
+        return this.delegate.scrape(format, includedNames);
+    }
+}
---
+++ src/test/java/com/tomw/azureexporter/PrometheusScrapeEndpointIntegrationTests.java
@@ -0,0 +1,36 @@
+package com.tomw.azureexporter;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureObservability
+public class PrometheusScrapeEndpointIntegrationTests {
+
+  private static final String ACTUATOR_PROMETHEUS_ENDPOINT = "/actuator/prometheus";
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testScrapeEndpoint_noParameters_ok() throws Exception {
+    mockMvc.perform(get(ACTUATOR_PROMETHEUS_ENDPOINT)).andExpect(status().isOk());
+  }
+
+  @Test
+  public void testScrapeEndpoint_noParameters_metricsReturned() throws Exception {
+    MvcResult result = mockMvc.perform(get(ACTUATOR_PROMETHEUS_ENDPOINT)).andReturn();
+    assertTrue(result.getResponse().getContentAsString().contains("cpu_usage"));
+  }
+
+}
