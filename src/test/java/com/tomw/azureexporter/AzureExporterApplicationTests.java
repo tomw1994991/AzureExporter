15f06b5941d0eb2ba67a782863d988727095ff16
Tom <T_o_m_white@hotmail.com>
2022-10-23 16:37:51 +0100

    Remove prometheusScrapeEndpointExtension.

--- src/test/java/com/tomw/azureexporter/AzureExporterApplicationTests.java
+++ src/test/java/com/tomw/azureexporter/AzureExporterApplicationTests.java
@@ -1,27 +1,16 @@
 package com.tomw.azureexporter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @AutoConfigureObservability
 class AzureExporterApplicationTests {
 
-    @Autowired
-    private PrometheusScrapeEndpointWebExtension promScrapeExtension;
-
-    @Autowired
-    private PrometheusScrapeEndpoint promScrapeDelegate;
 
     @Test
     void contextLoads() {
-        assertThat(promScrapeDelegate).isNotNull();
-        assertThat(promScrapeExtension).isNotNull();
     }
 
 }
