d33ece845a4b4479d16ea6b5e443e10468ca434c
Tom <T_o_m_white@hotmail.com>
2022-11-01 08:18:21 +0000

    Fix AzureExporterApplicationTests

--- src/test/java/com/tomw/azureexporter/AzureExporterApplicationTests.java
+++ src/test/java/com/tomw/azureexporter/AzureExporterApplicationTests.java
@@ -1,5 +1,7 @@
 package com.tomw.azureexporter;
 
+import io.prometheus.client.CollectorRegistry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @AutoConfigureObservability
 class AzureExporterApplicationTests {
+
+    @BeforeEach
+    public void setup(){
+        CollectorRegistry.defaultRegistry.clear();
+    }
 
     @Test
     void contextLoads() {
